bce0c891fe44cf4eba47585c67950b6816fb1dbe
BetulEseroglu <eserogluhaticebetul@gmail.com>
2023-08-24 00:30:33 +0300

    2408

--- lib/helper/dependencies.dart
+++ lib/helper/dependencies.dart
@@ -1,3 +1,16 @@
+import 'package:rota_yemek/controllers/popular_product_controller.dart';
+import 'package:rota_yemek/data/api/api_client.dart';
+import 'package:get/get.dart';
+import 'package:rota_yemek/data/repository/popular_product_repo.dart';
+
 Future<void> init()async{
+  //api client
+  Get.lazyPut(()=>ApiClient(appBaseUrl: "https://www.dbstech.com"));
+
+  //repos
+  Get.lazyPut(() => PopularProductRepo(apiClient: Get.find()));
+
+  //controllers
+  Get.lazyPut(() => PopularProductController(popularProductRepo: Get.find()));
 
 }
--- lib/main.dart
+++ lib/main.dart
@@ -2,10 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:rota_yemek/pages/food/popular_food_detail.dart';
 import 'package:rota_yemek/pages/food/recommended_food_detail.dart';
-import 'package:rota_yemek/pages/home/food_page_body.dart';
 import 'package:rota_yemek/pages/home/main_food_page.dart';
-
-void main() {
+import 'helper/dependencies.dart' as dep;
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await dep.init();
   runApp(const MyApp());
 }
 
